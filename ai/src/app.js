a91abc8a033b805fba3490822b14983ff411db0a
alireza arvandi <alirezaarvandi@alirezas-MacBook-Air.local>
2023-04-11 14:20:25 -0600

    ai

--- ai/src/app.js
+++ ai/src/app.js
@@ -1,1 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const port = 3000
+
+app.post('/ analyze ', async (req, res) => {
+    const { text } = req.body
+    res.send({ text, isRelatedToEmergency: true, tags: ['water'] })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening  on  port ${port}`)
+})
+
 console.log("omade inja to ai")

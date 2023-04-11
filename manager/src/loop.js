3bbf0070ead9bee8a1c57311688b9030b381f416
alireza arvandi <alirezaarvandi@alirezas-MacBook-Air.local>
2023-04-11 14:49:32 -0600

    bug fix

--- manager/src/loop.js
+++ manager/src/loop.js
@@ -5,7 +5,7 @@ const newTweetsQueue = new Queue('newTweets', 'redis://redis:6379');
 newTweetsQueue.process(async function (job, done) {
     const data = { job }
     try {
-        const result = await axios.get('localhost:5000/getTweets')
+        const result = await axios.get('http://localhost:5000/getTweets')
         console.log(data, result)
     } catch (error) {
         console.log(data, 'error newTweetsQueue', error)

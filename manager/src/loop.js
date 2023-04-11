a7bc523c549ef2dc45271b4d51427fef18b8534f
alireza arvandi <alirezaarvandi@alirezas-MacBook-Air.local>
2023-04-11 16:26:33 -0600

    bug fix

--- manager/src/loop.js
+++ manager/src/loop.js
@@ -8,16 +8,19 @@ const TweetTag = require('./model/TweetTag');
 const newTweetsQueue = new Queue('newTweets', 'redis://redis:6379');
 
 newTweetsQueue.process(async function (job, done) {
-    const data = job
+    const { data } = job
     try {
         const result = await axios.get('http://twitterbot:5000/getTweets')
         await Promise.map(result.data, async (tweet) => {
             const aiResult = await axios.post('http://ai:3000/analyze', { text: tweet.text })
             if (aiResult.data.isRelatedToEmergency) {
                 const newTweet = await Tweet.create({ text: tweet.text, tweetId: tweet.id })
+                console.log('newTweet', newTweet)
                 await Promise.map(aiResult.data.tags, async (tag) => {
                     const [newTag, created] = await Tag.findOrCreate({ where: { name: tag } })
+                    console.log('newTag', newTag)
                     await TweetTag.create({ tweetId: newTweet.id, tagId: newTag.id })
+                    console.log('newTweetTag', newTweetTag)
                 })
             }
         })

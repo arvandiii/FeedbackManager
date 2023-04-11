534b56a347f3ed1af922051ef6455cd9d46fb1df
alireza arvandi <alirezaarvandi@alirezas-MacBook-Air.local>
2023-04-11 16:32:18 -0600

    log added

--- ai/src/app.js
+++ ai/src/app.js
@@ -8,6 +8,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const port = 3000
 
+app.get('/', async (req, res) => {
+    res.send({hi: 'there'})
+})
+
 app.post('/analyze ', async (req, res) => {
     const { text } = req.body
     res.send({ text, isRelatedToEmergency: true, tags: ['water'] })
